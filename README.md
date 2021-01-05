10cadb799ea47762734d72faa12df0f2c2d45adf
Sobek <sobekcore@gmail.com>
2021-01-05 14:53:27 +0100

    Initial file boilerplate

---
+++ README.md
@@ -0,0 +1,3 @@
+## Work in progress!
+
+The NextJS continuous choosing package is not finished yet.
---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Index()
+{
+  return(
+    <div id="title">
+      NextJS continuous choosing <span id="boilerplate">(boilerplate)</span>
+    </div>
+  );
+}
